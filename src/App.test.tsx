95e06d26ddd19c85085662bde6c545cc48a283b3
Nabil Noh <19523841+mrnabilnoh@users.noreply.github.com>
2022-05-21 14:40:01 +0800

    test: add matchMedia mock function to App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,29 @@
-import React from "react"
-import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
-import { App } from "./App"
+import React from "react";
+import { screen } from "@testing-library/react";
+import { render } from "./test-utils";
+import { App } from "./App";
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
-})
+describe("React Application Component", () => {
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: (query: any) => {
+        return {
+          matches: false,
+          media: query,
+          onchange: null,
+          addListener: jest.fn(), // Deprecated
+          removeListener: jest.fn(), // Deprecated
+          addEventListener: jest.fn(),
+          removeEventListener: jest.fn(),
+          dispatchEvent: jest.fn(),
+        };
+      },
+    });
+  });
+  test("renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn chakra/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
